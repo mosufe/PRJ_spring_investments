4539a0c50d060f2eb3d03e1e47c3aecc606d0c32
Guilherme <g.jacome.paula@gmail.com>
2020-06-22 15:32:42 -0300

    Created Stock model, Repository and Interface . closes #7

---
+++ investor-data/src/main/java/br/com/datarich/investordata/models/Stock.java
@@ -0,0 +1,36 @@
+package br.com.datarich.investordata.models;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Guilherme Jacome de Paula
+ * Date 22/06/2020
+ * Time 15:25
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "stocks")
+@Builder
+public class Stock extends BaseEntity{
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "yield")
+    private String currentYield;
+
+    //TODO Add reference to Trade
+    //TODO Add reference to Company
+    //TODO Add reference to Enum StockCategory
+    //TODO Add reference to Enum StockType
+    //TODO Add reference to Company
+    //TODO Add reference to HistoricalData
+}
---
+++ investor-data/src/main/java/br/com/datarich/investordata/services/StockService.java
@@ -0,0 +1,12 @@
+package br.com.datarich.investordata.services;
+
+import br.com.datarich.investordata.models.Stock;
+
+/**
+ * Created by Guilherme Jacome de Paula
+ * Date 22/06/2020
+ * Time 15:31
+ */
+
+public interface StockService extends CrudService<Stock, Long>{
+}
