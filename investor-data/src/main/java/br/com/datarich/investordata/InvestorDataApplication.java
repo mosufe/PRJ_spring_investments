73b745836e24e9abe7d20628240f428037a86c0e
Guilherme <g.jacome.paula@gmail.com>
2020-06-22 14:19:01 -0300

    Created data-model project and web-initializer project. closes #3, #4

---
+++ investor-data/src/main/java/br/com/datarich/investordata/InvestorDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.datarich.investordata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestorDataApplication.class, args);
+    }
+
+}
---
+++ investor-web/src/main/java/br/com/datarich/investorweb/InvestorWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.datarich.investorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestorWebApplication.class, args);
+	}
+
+}
