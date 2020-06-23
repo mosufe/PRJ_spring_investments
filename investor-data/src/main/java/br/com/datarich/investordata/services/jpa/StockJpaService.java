8b43c5378db05b9ca90bfa1151aff90bf7d8eb9e
Guilherme <guilhermejp4@gmail.com>
2020-06-23 12:05:15 -0300

    Implemented Stock service plus specialized service methods. closes #14. Forgot to closes #13 in the last commit

---
+++ investor-data/src/main/java/br/com/datarich/investordata/services/jpa/StockJpaService.java
@@ -0,0 +1,44 @@
+package br.com.datarich.investordata.services.jpa;
+
+import br.com.datarich.investordata.models.Stock;
+import br.com.datarich.investordata.models.User;
+import br.com.datarich.investordata.repositories.StockRepository;
+import br.com.datarich.investordata.services.StockService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StockJpaService implements StockService {
+    private final StockRepository stockRepository;
+
+    public StockJpaService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Override
+    public Set<Stock> findAll() {
+        Set<Stock> stocks = new HashSet<>();
+        stockRepository.findAll().forEach(stocks::add);
+        return stocks;
+    }
+
+    @Override
+    public Stock findById(Long aLong) {
+        return stockRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Stock save(Stock object) {
+        return stockRepository.save(object);
+    }
+
+    @Override
+    public void delete(Stock object) {
+        stockRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        stockRepository.deleteById(aLong);
+    }
+}
