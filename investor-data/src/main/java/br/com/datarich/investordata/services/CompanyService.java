82ec6388f22b9d775066e3683537582881a9686f
Guilherme <guilhermejp4@gmail.com>
2020-06-23 12:21:19 -0300

    Implemented Company service plus specialized service methods. closes #15.

--- investor-data/src/main/java/br/com/datarich/investordata/services/CompanyService.java
+++ investor-data/src/main/java/br/com/datarich/investordata/services/CompanyService.java
@@ -1,9 +1,13 @@
 package br.com.datarich.investordata.services;
 
 import br.com.datarich.investordata.models.Company;
+import br.com.datarich.investordata.models.enums.CompanySize;
+import br.com.datarich.investordata.models.enums.Origin;
+import br.com.datarich.investordata.models.enums.Sector;
 import br.com.datarich.investordata.services.CrudService;
 
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Created by Guilherme Jacome de Paula
@@ -12,10 +16,9 @@ import java.util.Optional;
  */
 public interface CompanyService extends CrudService<Company, Long> {
 
-    Optional<Company> findByCountry();
-    Optional<Company> findBySector();
-    Optional<Company> findByCategory();
-    Optional<Company> findBySize();
+    Set<Company> findByCountry(Origin origin);
+    Set<Company> findBySector(Sector sector);
+    Set<Company> findBySize(CompanySize size);
 
     //TODO Implement additional finds
 }
