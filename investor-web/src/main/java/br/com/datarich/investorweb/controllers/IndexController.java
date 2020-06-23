eefc31bcc1895ad0119cf0c4f4cbe47de8718427
Guilherme <guilhermejp4@gmail.com>
2020-06-23 13:34:29 -0300

    Created Index page and Index Controller. Nothing special anout it

---
+++ investor-web/src/main/java/br/com/datarich/investorweb/controllers/IndexController.java
@@ -0,0 +1,11 @@
+package br.com.datarich.investorweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String index() { return "index"; }
+}
