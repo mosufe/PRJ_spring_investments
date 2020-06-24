57aabb8d1aa4d88af3a56dfbda43d3c89a1bda4d
Guilherme <guilhermejp4@gmail.com>
2020-06-24 16:55:51 -0300

    Corrigido erros de testes do circleCI incluindo a anotacao @ExtendWith antes de cada classe.

--- investor-web/src/test/java/br/com/datarich/investorweb/InvestorWebApplicationTests.java
+++ investor-web/src/test/java/br/com/datarich/investorweb/InvestorWebApplicationTests.java
@@ -1,8 +1,11 @@
 package br.com.datarich.investorweb;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith({SpringExtension.class})
 @SpringBootTest
 class InvestorWebApplicationTests {
 
