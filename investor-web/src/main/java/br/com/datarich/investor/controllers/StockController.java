636dcc0335cc8f86243bee2bbf727ba0b82f7991
Guilherme <g.jacome.paula@gmail.com>
2020-06-25 17:35:34 -0300

    Created Stocks index listing page. closes #24

--- investor-web/src/main/java/br/com/datarich/investor/controllers/StockController.java
+++ investor-web/src/main/java/br/com/datarich/investor/controllers/StockController.java
@@ -1,12 +1,23 @@
 package br.com.datarich.investor.controllers;
 
+import br.com.datarich.investor.repositories.StockRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/stocks")
 public class StockController {
 
+    private final StockRepository stockRepository;
+
+    public StockController(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
     @RequestMapping({"", "/", "/index.html"})
-    public String index(){ return "stocks/index";}
+    public String index(Model model){
+        model.addAttribute("stocks", stockRepository.findAll());
+        return "stocks/index";
+    }
 }
