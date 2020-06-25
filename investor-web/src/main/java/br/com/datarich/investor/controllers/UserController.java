08e22fbb1b1c2f4f0dafa61dd302fde33c3ebda9
Guilherme <g.jacome.paula@gmail.com>
2020-06-25 17:47:07 -0300

    Created Users index listing page. closes #22

--- investor-web/src/main/java/br/com/datarich/investor/controllers/UserController.java
+++ investor-web/src/main/java/br/com/datarich/investor/controllers/UserController.java
@@ -1,14 +1,24 @@
 package br.com.datarich.investor.controllers;
 
+import br.com.datarich.investor.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String index(){
+    public String index(Model model)
+    {
+        model.addAttribute("users", userRepository.findAll());
         return "users/index";
     }
 }
