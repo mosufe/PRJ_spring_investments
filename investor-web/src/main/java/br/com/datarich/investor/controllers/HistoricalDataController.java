504701940cfe73d53f87ecae4fcf596eadcacb67
Guilherme <g.jacome.paula@gmail.com>
2020-06-25 17:27:49 -0300

    Created Historical data index listing page. closes #25

--- investor-web/src/main/java/br/com/datarich/investor/controllers/HistoricalDataController.java
+++ investor-web/src/main/java/br/com/datarich/investor/controllers/HistoricalDataController.java
@@ -1,14 +1,23 @@
 package br.com.datarich.investor.controllers;
 
+import br.com.datarich.investor.repositories.HistoricalDataRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/historical-data")
 public class HistoricalDataController {
 
+    private final HistoricalDataRepository historicalDataRepository;
+
+    public HistoricalDataController(HistoricalDataRepository historicalDataRepository) {
+        this.historicalDataRepository = historicalDataRepository;
+    }
+
     @RequestMapping({"","/", "/index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("historicals", historicalDataRepository.findAll());
         return "historical-data/index";
     }
 }
