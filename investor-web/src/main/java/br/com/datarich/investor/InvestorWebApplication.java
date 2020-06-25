ff070d8bdc14b441ae825ebd5ae3a5eeac238b05
Guilherme <g.jacome.paula@gmail.com>
2020-06-25 17:23:02 -0300

    Created Trade index listing page. closes #26

--- investor-web/src/main/java/br/com/datarich/investor/InvestorWebApplication.java
+++ investor-web/src/main/java/br/com/datarich/investor/InvestorWebApplication.java
@@ -11,5 +11,4 @@ public class InvestorWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvestorWebApplication.class, args);
 	}
-
 }
--- investor-web/src/main/java/br/com/datarich/investor/controllers/TradeController.java
+++ investor-web/src/main/java/br/com/datarich/investor/controllers/TradeController.java
@@ -1,12 +1,23 @@
 package br.com.datarich.investor.controllers;
 
+import br.com.datarich.investor.repositories.TradeRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/trades")
 public class TradeController {
 
+    private final TradeRepository tradeRepository;
+
+    public TradeController(TradeRepository tradeRepository) {
+        this.tradeRepository = tradeRepository;
+    }
+
     @RequestMapping({"", "/", "/index.html"})
-    public String index(){ return "trades/index";}
+    public String index(Model model){
+        model.addAttribute("trades", tradeRepository.findAll());
+        return "trades/index";
+    }
 }
